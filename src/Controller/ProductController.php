117d1359d93d43a551f954dec67685ac57def9f6
ikolan <ikolan@users.noreply.github.com>
2022-10-20 11:30:31 +0200

    Adding basic product cards

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,20 @@
 
 namespace App\Controller;
 
+use App\Model\ProductManager;
+use App\Model\UserManager;
+
 class ProductController extends AbstractController
 {
     public function index(): string
     {
-        return $this->twig->render("Product/index.html.twig");
+        $products = (new ProductManager())->selectAll('date', 'DESC');
+
+        foreach ($products as &$product) {
+            $user = (new UserManager())->selectOneById($product["user_id"]);
+            $product["user_pseudo"] = $user["pseudo"];
+        }
+
+        return $this->twig->render("Product/index.html.twig", ["products" => $products]);
     }
 }
