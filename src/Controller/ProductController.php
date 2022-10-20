a32bdd74a63a9812f03319d776240c64842a3bd5
ikolan <ikolan@users.noreply.github.com>
2022-10-20 18:01:00 +0200

    Unify SQL calls for the list of products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,20 +3,12 @@
 namespace App\Controller;
 
 use App\Model\ProductManager;
-use App\Model\UserManager;
 
 class ProductController extends AbstractController
 {
     public function index(): string
     {
-        $products = (new ProductManager())->selectAll('date', 'DESC');
-
-        foreach ($products as &$product) {
-            $user = (new UserManager())->selectOneById($product["user_id"]);
-            $product["user_pseudo"] = $user["pseudo"];
-            $product["user_rating"] = $user["rating"];
-            $product["user_photo"] = $user["photo"];
-        }
+        $products = (new ProductManager())->selectPageWithUser();
 
         return $this->twig->render("Product/index.html.twig", ["products" => $products]);
     }
--- src/Model/ProductManager.php
+++ src/Model/ProductManager.php
@@ -5,4 +5,18 @@ namespace App\Model;
 class ProductManager extends AbstractManager
 {
     public const TABLE = "product";
+
+    public function selectPageWithUser($page = 1, $limit = 20, $orderBy = 'date', $direction = "DESC"): array
+    {
+        $offset = ($page - 1) * $limit;
+
+        $query = "SELECT p.*, u.pseudo as user_pseudo, u.photo as user_photo, u.rating as user_rating";
+        $query .= " FROM product p JOIN user u ON p.user_id = u.id";
+        if ($orderBy) {
+            $query .= " ORDER BY " . $orderBy . " " . $direction;
+        }
+        $query .= " LIMIT " . $offset . ", " . $limit;
+
+        return $this->pdo->query($query)->fetchAll();
+    }
 }
