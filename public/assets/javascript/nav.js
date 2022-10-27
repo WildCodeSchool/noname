6daeae1a811b3bc77739d602e760f802ddb77d65
ikolan <ikolan@users.noreply.github.com>
2022-10-27 15:37:27 +0200

    Basic logout

--- public/assets/javascript/nav.js
+++ public/assets/javascript/nav.js
@@ -8,3 +8,12 @@ menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 });
+
+// Login form
+
+const loginBtn = document.querySelector('.loginButton');
+const loginForm = document.querySelector('.loginPrompt');
+
+loginBtn.addEventListener('click', function() {
+    loginForm.classList.toggle('showed');
+});
