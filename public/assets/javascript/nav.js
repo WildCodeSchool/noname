7b1b39a76b5184bba94adcf4ba5352e7b59f5ee9
ikolan <ikolan@users.noreply.github.com>
2022-10-27 16:24:07 +0200

    Basic responsive

--- public/assets/javascript/nav.js
+++ public/assets/javascript/nav.js
@@ -1,19 +1,20 @@
-
 // Burger button
 
-const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav');
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
 
-menu_btn.addEventListener('click', function () {
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
+menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
 });
 
 // Login form
 
-const loginBtn = document.querySelector('.loginButton');
-const loginForm = document.querySelector('.loginPrompt');
+const loginBtn = document.querySelectorAll(".loginButton");
+const loginForm = document.querySelector(".loginPrompt");
 
-loginBtn.addEventListener('click', function() {
-    loginForm.classList.toggle('showed');
-});
+loginBtn.forEach((node) =>
+    node.addEventListener("click", function () {
+        loginForm.classList.toggle("showed");
+    })
+);
