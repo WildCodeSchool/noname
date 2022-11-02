501bb4a9870a39ef269bd03559e950c38451ebfd
ikolan <ikolan@users.noreply.github.com>
2022-11-02 11:05:41 +0100

    Adding a better (cross) close search panel button

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -1,11 +1,11 @@
 const searchPanelButton = document.getElementById("searchPanelButton");
 const searchPanel = document.getElementById("searchPanel");
-const closeSearchPanelLink = document.getElementById("closeSearchPanelLink");
+const closeSearchPanelButton = document.getElementById("closeSearchPanelButton");
 
 searchPanelButton.onclick = () => {
     searchPanel.classList.add("isOpen");
 }
 
-closeSearchPanelLink.onclick = () => {
+closeSearchPanelButton.onclick = () => {
     searchPanel.classList.remove("isOpen");
 }
