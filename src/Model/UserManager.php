0583d78352109fdc7f7617b048b3c82bfcd42944
td6904 <td665@pm.me>
2022-11-02 17:23:15 +0100

    Gitignore updatd, logo also sends me back to home page on click

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -5,4 +5,16 @@ namespace App\Model;
 class UserManager extends AbstractManager
 {
     public const TABLE = 'user';
+   /*Everything needed
+    id 
+    pseudo 
+    lastname 
+    firstname 
+    adress 
+    email 
+    phone_number 
+    photo 
+    rating 
+    is_admin 
+    */
 }
