b429cfcdb6da84ba2f22b0519f4973bed19dc21a
ikolan <ikolan@users.noreply.github.com>
2022-11-03 11:40:58 +0100

    Adapt the navbar to user connection

--- public/assets/javascript/nav.js
+++ public/assets/javascript/nav.js
@@ -1,19 +1,21 @@
 // Burger button
 
+const urlParams = new URLSearchParams(window.location.search);
 const menu_btn = document.querySelector(".hamburger");
 const mobile_menu = document.querySelector(".mobile-nav");
-
-menu_btn.addEventListener("click", function () {
-    menu_btn.classList.toggle("is-active");
-    mobile_menu.classList.toggle("is-active");
-});
-
-// Login form
-
-const urlParams = new URLSearchParams(window.location.search);
 const loginBtn = document.querySelectorAll(".loginButton");
 const loginPrompt = document.querySelector(".loginPrompt");
 const logoutPrompt = document.querySelector(".logoutPrompt");
+
+menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    if (mobile_menu !== null) {
+        mobile_menu.classList.toggle("is-active");
+    }
+    if (loginPrompt !== null) {
+        loginPrompt.classList.toggle("showed");
+    }
+});
 
 loginBtn.forEach((node) =>
     node.addEventListener("click", function () {
@@ -29,5 +31,6 @@ loginBtn.forEach((node) =>
 if (urlParams.get("loginFailed") === "true") {
     if (loginPrompt !== null) {
         loginPrompt.classList.toggle("showed");
+        menu_btn.classList.toggle("is-active");
     }
 }
