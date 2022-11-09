67da246ae7202c1f134afe07387a8cc898e2ec9f
ikolan <ikolan@users.noreply.github.com>
2022-11-09 15:16:07 +0100

    More fixes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,42 +9,46 @@ class UserController extends AbstractController
 //---------------Signup---------------------------------------
 
     //Connection to twig template
-    public function signUp(): string
+    public function signUp(): ?string
     {
+        if (is_null($this->user)) {
+            $errors = [];
 
-        $errors = [];
+            $user = $_POST;
+            $userManager = new UserManager();
 
-        $user = $_POST;
-        $userManager = new UserManager();
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //Photo uploading
+                $uploadDir = 'uploads/';
+                $uploadFilePhoto = $uploadDir . basename($_FILES['photo']['name']);
+                $user['photo'] = $uploadFilePhoto;
+                move_uploaded_file($_FILES['photo']['tmp_name'], $uploadFilePhoto);
+            //Making sure certain fields start with capital letter
+                $user['firstname'] =  ucfirst($user['firstname']);
+                $user['lastname'] = ucfirst($user['lastname']);
+            //Cleaning $_POST data
+                $user  = array_map('trim', $user);
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        //Photo uploading
-            $uploadDir = 'uploads/';
-            $uploadFilePhoto = $uploadDir . basename($_FILES['photo']['name']);
-            $user['photo'] = $uploadFilePhoto;
-            move_uploaded_file($_FILES['photo']['tmp_name'], $uploadFilePhoto);
-        //Making sure certain fields start with capital letter
-            $user['firstname'] =  ucfirst($user['firstname']);
-            $user['lastname'] = ucfirst($user['lastname']);
-        //Cleaning $_POST data
-            $user  = array_map('trim', $user);
+                $errors = $this->verifyData($user);
 
-            $errors = $this->verifyData($user);
+                if (!empty($errors)) {
+                    return $this->twig->render('signup.html.twig', [
+                    "errors" => $errors,
+                    ]);
+                }
 
-            if (!empty($errors)) {
-                return $this->twig->render('signup.html.twig', [
-                "errors" => $errors,
-                ]);
+                $userId = $userManager->createUser($user);
+                $_SESSION["user_id"] = $userId;
+                header("Location: /");
             }
 
-            $userId = $userManager->createUser($user);
-            $_SESSION["user_id"] = $userId;
-            header("Location: /");
+            return $this->twig->render('signup.html.twig', [
+            'errors' => $errors
+            ]);
         }
 
-        return $this->twig->render('signup.html.twig', [
-        'errors' => $errors
-        ]);
+        header("Location: /");
+        return null;
     }
 
     private function verifyData($user): array
